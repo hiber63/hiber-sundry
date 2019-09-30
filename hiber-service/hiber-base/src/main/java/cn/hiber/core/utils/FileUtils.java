719af4e0832fe9cbc26e84b130b791f18275ff5e
hiber63 <39085603@qq.com>
2019-09-30 23:00:27 +0800

    FileUtils

---
+++ hiber-service/hiber-base/src/main/java/cn/hiber/core/utils/FileUtils.java
@@ -0,0 +1,53 @@
+package cn.hiber.core.utils;
+
+import java.io.*;
+
+/**
+ * @author hiber
+ */
+public class FileUtils {
+
+	public static String addPath(String... paths) {
+		StringBuffer ret = new StringBuffer();
+		if(paths != null && paths.length>=1) {
+			for(int i=0,len=paths.length; i<len; i++) {
+				ret.append(paths[i]).append(i==len-1?"":File.separator);
+			}
+		}
+		return ret.toString();
+ 	}
+
+	public static void close(Closeable... cs) {
+		if(cs != null && cs.length > 0) {
+			for(Closeable c:cs) {
+				close(c);
+			}
+		}
+	}
+
+	public static void close(Closeable c) {
+		if (c != null) {
+			try {
+				c.close();
+			} catch (IOException e) {
+				System.err.println(e);
+			}
+			c = null;
+		}
+	}
+
+	public static void uploadFile(byte[] file, String filePath, String fileName) throws Exception{
+		FileOutputStream out = null;
+		try {
+			File targetFile = new File(filePath);
+			if(!targetFile.exists()){
+				targetFile.mkdirs();
+			}
+			out = new FileOutputStream(filePath+fileName);
+			out.write(file);
+			out.flush();
+		} finally {
+			close(out);
+		}
+	}
+}
