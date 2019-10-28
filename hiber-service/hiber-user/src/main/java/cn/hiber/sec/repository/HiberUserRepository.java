067f1dc7a4d5747fb88b644af6a72a10fefe63b3
hiber63 <39085603@qq.com>
2019-10-28 23:52:31 +0800

    HiberUserRepository update

--- hiber-service/hiber-user/src/main/java/cn/hiber/sec/repository/HiberUserRepository.java
+++ hiber-service/hiber-user/src/main/java/cn/hiber/sec/repository/HiberUserRepository.java
@@ -1,12 +1,12 @@
 package cn.hiber.sec.repository;
 
-import cn.hiber.core.repository.BaseRepository;
+import cn.hiber.core.repository.StringRepository;
 import cn.hiber.sec.entity.HiberUser;
 
 /**
  * @author hiber
  */
-public interface HiberUserRepository extends BaseRepository<HiberUser,String> {
+public interface HiberUserRepository extends StringRepository<HiberUser> {
 
     HiberUser findByUsername(String username);
 
