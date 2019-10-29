a58504df5c3d66b775d8f92aaf455f7a05e70ea0
hiber63 <39085603@qq.com>
2019-10-29 16:37:23 +0800

    init HiberContext

---
+++ hiber-service/hiber-base/src/main/java/cn/hiber/core/context/HiberContext.java
@@ -0,0 +1,26 @@
+package cn.hiber.core.context;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author hiber
+ */
+@Component
+@Lazy(value = false)
+public class HiberContext implements ApplicationContextAware {
+    private static ApplicationContext APPLICATION_CONTEXT;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        APPLICATION_CONTEXT = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return APPLICATION_CONTEXT;
+    }
+
+}
