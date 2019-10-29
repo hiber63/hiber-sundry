dcf291facfae27667938e2e215ea7377b609af76
hiber63 <39085603@qq.com>
2019-10-30 01:19:02 +0800

    init HiberCode

---
+++ hiber-service/hiber-user/src/main/java/cn/hiber/sec/entity/HiberCode.java
@@ -0,0 +1,34 @@
+package cn.hiber.sec.entity;
+
+import cn.hiber.core.entity.StringEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author hiber
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@DynamicInsert
+@DynamicUpdate
+@Entity
+@Table(name = "hiber_sec_code")
+public class HiberCode extends StringEntity {
+
+    private String code;
+
+    private String name;
+
+    private String remark;
+
+
+}
---
+++ hiber-service/hiber-user/src/main/java/cn/hiber/sec/repository/HiberCodeRepository.java
@@ -0,0 +1,11 @@
+package cn.hiber.sec.repository;
+
+import cn.hiber.core.repository.StringRepository;
+import cn.hiber.sec.entity.HiberCode;
+
+/**
+ * @author hiber
+ */
+public interface HiberCodeRepository extends StringRepository<HiberCode> {
+
+}
